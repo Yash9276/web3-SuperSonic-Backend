635d0b36260dddd15cf27de375962bbccae3ff38
Yash9276 <kudnaryash@gmail.com>
2025-11-24 02:30:03 +0530

    stS and OS liquid tokens added

--- web3-supersonic-backend/contracts/Mytoken1.sol
+++ web3-supersonic-backend/contracts/Mytoken1.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Mytoken1 is ERC20 {
-    constructor() ERC20("Kryptonite", "stSEI") {
+    constructor() ERC20("Origin Sonic", "OS") {
     }
      function mint(address to, uint256 amount) external {
         _mint(to, amount);
